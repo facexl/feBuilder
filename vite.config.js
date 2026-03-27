61623ca45f2be509bd7d3249eceee62865a6f24e
肖浪 <xiaolang@xiaolangdeMacBook-Air.local>
2026-03-27 10:29:14 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const noHmr = process.env.NO_HMR === 'true';
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 9898,
-    // hmr: noHmr ? false : undefined,
-    // watch: noHmr ? null : undefined,
+    hmr:false,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
